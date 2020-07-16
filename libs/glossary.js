85c5bb56474c09a33f17a25290a1c3bc384b9e33
pierre.bouteloup <p.bouteloup@b-p.fr>
2020-07-16 16:29:35 +0200

    trying to handle windows-1252 encoding

--- libs/glossary.js
+++ libs/glossary.js
@@ -1,15 +1,43 @@
 const fs = require('fs');
+const {TextDecoder} = require('util');
 
 const csv = require('csv-parser');
-const { resolve } = require('path');
+const jschardet = require('jschardet');
+const windows1252 = require('windows-1252');
+
+const decoders = {};
 
 async function glossary(file, output) {
   const results = [];
 
+  const buff = fs.readFileSync(file);
+
+  let {encoding} = jschardet.detect(buff);
+
+  if (!encoding) {
+    return ;
+  }
+
+  if (!decoders[encoding]) {
+    if (encoding === 'windows-1252') {
+      decoders[encoding] = {
+        decode(buff) {
+          return windows1252.decode(buff.toString('binary'));
+        }
+      };
+    } else {
+      decoders[encoding] = new TextDecoder(encoding);
+    }
+  }
+
   await new Promise((resolve, reject) => {
     fs.createReadStream(file)
-      .pipe(csv({separator: ';', headers: ['title', 'keywords']}))
-      .on('data', data => {
+      .pipe(csv({separator: ';', raw: true, headers: ['title', 'keywords']}))
+      .on('data', raw => {
+        const data = {
+          title: decoders[encoding].decode(raw.title),
+          keywords: decoders[encoding].decode(raw.keywords),
+        };
         const keywords = data.keywords.split(';');
         keywords.forEach(keyword => {
           results.push(`${data.title};${keyword}`);
@@ -19,7 +47,13 @@ async function glossary(file, output) {
       .on('error', reject);
   });
 
-  fs.writeFileSync(output, results.join('\n'));
+  const str = results.join('\n');
+
+  if (encoding === 'windows-1252') {
+    fs.writeFileSync(output, windows1252.encode(str));
+  } else {
+    fs.writeFileSync(output, str);
+  }
 }
 
 module.exports = {glossary};
